f2c8b8da0b68334516ca46f0eae6574237e50684
“Ymyysimida“ <2753212090@qq.com>
2020-08-09 20:21:53 +0800

    数据库  promise

--- 05/mysql-demo/demo.js
+++ 05/mysql-demo/demo.js
@@ -1,19 +1,21 @@
-// 引入mysql包
-var mysql = require('mysql');
-// 1.创建连接
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',	//本机
-    user     : 'me',	//账号root
-    password : 'secret',	//密码12345
-    database : 'my_db'	//数据库名
+    host     : 'localhost',
+    user     : 'root',
+    password : '990522',
+    database : 'users'
 });
-// 2.连接数据库	（打开冰箱门）
+
 connection.connect();
-//3.执行数据操作	（把大象放到冰箱）
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;//抛出异常阻止代码往下执行
-    // 没有异常打印输出结果
-    console.log('The solution is: ',results);
+
+connection.query('SELECT * FROM `user`', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
 });
-//4.关闭连接	（关闭冰箱门）
+
+// connection.query('INSERT INTO user VALUES(null,"admin","123456")', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results);
+// });
+
 connection.end();
